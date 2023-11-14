e8ccf2e6295be7fea1fff077a95adef9c820fb29
MichaelTang <mike.g.tang@gmail.com>
2023-11-14 12:56:32 +0800

    link act by range

--- role.link.js
+++ role.link.js
@@ -11,31 +11,36 @@ var roleLink = {
     /** @param {link} link **/
     run: function () {
         //----W2N27----
-        var link1 = Game.getObjectById('650d9b1ee851c93a8cd7f1b4');
-        var link2 = Game.getObjectById('650da9af3bfac34617fd5fbb');
-        var link3 = Game.getObjectById('65290b5e72ae07cb844563a1');
-        if (link1.store[RESOURCE_ENERGY] >= 200 && link2.store.getFreeCapacity(RESOURCE_ENERGY) > 200) {
+        let link1 = Game.getObjectById('650d9b1ee851c93a8cd7f1b4');
+        let link2 = Game.getObjectById('650da9af3bfac34617fd5fbb');
+        let link3 = Game.getObjectById('65290b5e72ae07cb844563a1');
+        let range1 = link1.pos.getRangeTo(link2);
+        let range2 = link1.pos.getRangeTo(link3);
+        if (link1.store[RESOURCE_ENERGY] >= range1*10 && link2.store.getFreeCapacity(RESOURCE_ENERGY) > range1*10) {
             link1.transferEnergy(link2);
         }
-        else if (link1.store[RESOURCE_ENERGY] >= 200 && link3.store.getFreeCapacity(RESOURCE_ENERGY) > 200) {
+        else if (link1.store[RESOURCE_ENERGY] >= range2*10 && link3.store.getFreeCapacity(RESOURCE_ENERGY) > range2*10) {
             link1.transferEnergy(link3);
         }
         //----W1N27----
-        var linkb1 = Game.getObjectById('6521fb1a11986b19ab27b052');
-        var linkb2 = Game.getObjectById('65376704c6ed5f491c664414');
-        if (linkb1.store[RESOURCE_ENERGY] >= 200 && linkb2.store.getFreeCapacity(RESOURCE_ENERGY) > 200) {
+        let linkb1 = Game.getObjectById('6521fb1a11986b19ab27b052');
+        let linkb2 = Game.getObjectById('65376704c6ed5f491c664414');
+        let rangeb1 = linkb1.pos.getRangeTo(linkb2);
+        if (linkb1.store[RESOURCE_ENERGY] >= rangeb1*10 && linkb2.store.getFreeCapacity(RESOURCE_ENERGY) > rangeb1*10) {
             linkb1.transferEnergy(linkb2);
         }
         //----W4N28----
-        var linkc1 = Game.getObjectById('653378d4e834a8635f892808');
-        var linkc2 = Game.getObjectById('6533a1a7e207c41ca2476de1');
-        if (linkc1.store[RESOURCE_ENERGY] >= 200 && linkc2.store.getFreeCapacity(RESOURCE_ENERGY) > 200) {
+        let linkc1 = Game.getObjectById('653378d4e834a8635f892808');
+        let linkc2 = Game.getObjectById('6533a1a7e207c41ca2476de1');
+        let rangec1 = linkc1.pos.getRangeTo(linkc2);
+        if (linkc1.store[RESOURCE_ENERGY] >= rangec1*10 && linkc2.store.getFreeCapacity(RESOURCE_ENERGY) > rangec1*10) {
             linkc1.transferEnergy(linkc2);
         }
         //----W5N28----
-        var linkd1 = Game.getObjectById('6549998cce5f31cfe3c5f31a');
-        var linkd2 = Game.getObjectById('654df541cbb618a55ddfb95c');
-        if (linkd1.store[RESOURCE_ENERGY] >= 200 && linkd2.store.getFreeCapacity(RESOURCE_ENERGY) > 200) {
+        let linkd1 = Game.getObjectById('6549998cce5f31cfe3c5f31a');
+        let linkd2 = Game.getObjectById('654df541cbb618a55ddfb95c');
+        let ranged1 = linkd1.pos.getRangeTo(linkd2);
+        if (linkd1.store[RESOURCE_ENERGY] >= ranged1*10 && linkd2.store.getFreeCapacity(RESOURCE_ENERGY) > ranged1*10) {
             linkd1.transferEnergy(linkd2);
         }
     }
